2a0bc9d797767ee3c9db47572449b77007076722
Zen Master SoSo <zen_master_soso@dridesign.sh.cn>
2025-08-21 13:54:49 +0800

    Bump version 1.4.0 -> 1.4.1

--- simple_carla/__init__.py
+++ simple_carla/__init__.py
@@ -236,7 +236,7 @@ from carla_backend import (
 from resources_rc import qCleanupResources
 
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 
 
 # -------------------------------------------------------------------
